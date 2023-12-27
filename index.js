d3396726668a881a5ba8240a51185e4f2235b6f7
hari6091 <hari.junior19@gmail.com>
2023-12-27 15:22:02 -0300

    feat: filter get rows

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
 app.get("/values/all", async (req, res) => {
   const values = await pgClient.query("SELECT * FROM values");
 
-  res.send(values);
+  res.send(values.rows);
 });
 
 app.post("/values", async (req, res) => {
